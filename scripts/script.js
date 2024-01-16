e3ddba78e852505f2a828064305a11d7ba844ca1
MazurkiewiczR <mazurkiewicz.er@gmail.com>
2024-01-16 21:00:44 +0100

    slider added

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,74 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.toggle("no-scroll");
   });
 });
+
+var slider = new KeenSlider(
+  "#my-keen-slider",
+  {
+    mode: "snap",
+    loop: "true",
+
+    slides: () => [
+      {
+        size: 0.8,
+        spacing: 1,
+        origin: 0.1,
+      },
+      {
+        size: 0.8,
+        spacing: 1,
+        origin: 0.1,
+      },
+      {
+        size: 0.8,
+        spacing: 1,
+        origin: 0.1,
+      },
+      {
+        size: 0.8,
+        spacing: 1,
+        origin: 0.1,
+      },
+      {
+        size: 0.8,
+        spacing: 1,
+        origin: 0.1,
+      },
+      {
+        size: 0.8,
+        spacing: 1,
+        origin: 0.1,
+      },
+    ],
+  },
+  [
+    (slider) => {
+      let timeout;
+      let mouseOver = false;
+      function clearNextTimeout() {
+        clearTimeout(timeout);
+      }
+      function nextTimeout() {
+        clearTimeout(timeout);
+        if (mouseOver) return;
+        timeout = setTimeout(() => {
+          slider.next();
+        }, 2000);
+      }
+      slider.on("created", () => {
+        slider.container.addEventListener("mouseover", () => {
+          mouseOver = true;
+          clearNextTimeout();
+        });
+        slider.container.addEventListener("mouseout", () => {
+          mouseOver = false;
+          nextTimeout();
+        });
+        nextTimeout();
+      });
+      slider.on("dragStarted", clearNextTimeout);
+      slider.on("animationEnded", nextTimeout);
+      slider.on("updated", nextTimeout);
+    },
+  ]
+);
